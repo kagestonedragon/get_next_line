fa25212e1a65b21abdfda5a6405e4792249b9079
Emerita Medea <emedea@oa-n5.21-school.ru>
2019-05-20 15:17:09 +0300

    final gnl

--- get_next_line.c
+++ get_next_line.c
@@ -1,57 +1,80 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emedea <emedea@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/18 12:11:51 by emedea            #+#    #+#             */
+/*   Updated: 2019/05/20 15:13:42 by emedea           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
-#include <string.h>
-#define BUFF_SIZE 32
+#include <fcntl.h>
+#include <unistd.h>
+#include "libft/includes/libft.h"
+#include "get_next_line.h"
 
-char            *ft_string_realloc(char *input, int size)
+static char		**check_empty_string(char **stack)
 {
-    char        *output;
-
-    if (!input)
-        output = (char *)malloc(sizeof(char) * (size + 1));
-    else
-    {
-        output = (char *)malloc(sizeof(char) * (size + 1));
-        bzero(output, (size + 1));
-        strcpy(output, input);
-        free(input);
-    }
-    return (output);
+	if (!(*stack))
+		*stack = ft_strnew(0);
+	return (stack);
 }
 
-int             get_next_line(const int fd, char **line)
+static int		generate_new_line(char **stack, char **line)
 {
-    static char *stack[10240];
-    char        *pointer;
-    char        buffer[BUFF_SIZE + 1];
-    int         amount;
+	char		*temporary;
 
-    bzero(buffer, BUFF_SIZE + 1);
-    if (fd < 0)
-        return (0);
-    while (amount = read(fd, buffer, BUFF_SIZE + 1))
-    {
-        stack[fd] = ft_string_realloc(stack[fd], strlen(stack[fd]) + amount);
-        strcat(stack[fd], buffer);
-        if (pointer = strchr(stack[fd], '\n'))
-        {
-            *pointer = '\0';
-            ft_string_realloc(*line, strlen(stack[fd]));
-            return (1);
-        }
-    }
-    return (0);
+	if (!(*stack)[0])
+		return (0);
+	if (ft_strchr(*stack, '\n'))
+	{
+		*(ft_strchr(*stack, '\n')) = '\0';
+		*line = ft_strdup(*stack);
+		temporary = ft_strdup(ft_strchr(*stack, '\0') + 1);
+		free(*stack);
+		if (temporary)
+		{
+			*stack = ft_strdup(temporary);
+			free(temporary);
+		}
+	}
+	else
+	{
+		*line = ft_strdup(*stack);
+		ft_memdel((void **)stack);
+	}
+	return (1);
 }
 
-int             main(int argc, char **argv)
+static void		generate_new_string(char **stack, char *buffer)
 {
-    int         fd;
-    char        *line;
+	char		*temporary;
 
-    fd = open(argv[1], O_RDONLY);
-    get_next_line(fd, &line);
-    printf("%s", line);
-    return (argc);
+	temporary = ft_strjoin(*stack, buffer);
+	free(*stack);
+	*stack = ft_strdup(temporary);
+	free(temporary);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static char	*stack[FD_MAX];
+	char		buffer[BUFF_SIZE + 1];
+	int			amount;
+
+	if (fd < 0 || read(fd, buffer, 0) || !line || fd > FD_MAX)
+		return (-1);
+	check_empty_string(&(stack[fd]));
+	while (!(ft_strchr(stack[fd], '\n')))
+	{
+		amount = read(fd, buffer, BUFF_SIZE);
+		buffer[amount] = '\0';
+		if (!amount)
+			break ;
+		generate_new_string(&(stack[fd]), buffer);
+	}
+	return (generate_new_line(&(stack[fd]), line));
 }
