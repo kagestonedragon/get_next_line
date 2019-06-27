6af589ec25529ec68ee1ea2b9a2c16518f0e4c2c
kagestonedragon <kagestonedragon@gmail.com>
2019-06-27 18:26:17 +0300

    up

--- get_next_line.c
+++ get_next_line.c
@@ -16,54 +16,54 @@
 #include "libft/includes/libft.h"
 #include "get_next_line.h"
 
-static char		**check_empty_string(char **stack)
+static char     **check_empty_string(char **stack)
 {
-	if (!(*stack))
-		*stack = ft_strnew(0);
-	return (stack);
+    if (!(*stack))
+        *stack = ft_strnew(0);
+    return (stack);
 }
 
-static int		generate_new_line(char **stack, char **line)
+static int      generate_new_line(char **stack, char **line)
 {
-	char		*temporary;
+	char        *temporary;
 
-	if (!(*stack)[0])
-		return (0);
-	if (ft_strchr(*stack, '\n'))
-	{
-		*(ft_strchr(*stack, '\n')) = '\0';
-		*line = ft_strdup(*stack);
-		temporary = ft_strdup(ft_strchr(*stack, '\0') + 1);
-		free(*stack);
-		if (temporary)
-		{
-			*stack = ft_strdup(temporary);
-			free(temporary);
-		}
-	}
-	else
-	{
-		*line = ft_strdup(*stack);
-		ft_memdel((void **)stack);
-	}
-	return (1);
+    if (!(*stack)[0])
+        return (0);
+    if (ft_strchr(*stack, '\n'))
+    {
+        *(ft_strchr(*stack, '\n')) = '\0';
+        *line = ft_strdup(*stack);
+        temporary = ft_strdup(ft_strchr(*stack, '\0') + 1);
+        free(*stack);
+        if (temporary)
+        {
+            *stack = ft_strdup(temporary);
+            free(temporary);
+        }
+    }
+    else
+    {
+        *line = ft_strdup(*stack);
+        ft_memdel((void **)stack);
+    }
+    return (1);
 }
 
-static void		generate_new_string(char **stack, char *buffer)
+static void     generate_new_string(char **stack, char *buffer)
 {
-	char		*temporary;
+	char        *temporary;
 
-	temporary = ft_strjoin(*stack, buffer);
-	free(*stack);
-	*stack = ft_strdup(temporary);
-	free(temporary);
+    temporary = ft_strjoin(*stack, buffer);
+    free(*stack);
+    *stack = ft_strdup(temporary);
+    free(temporary);
 }
 
-int				get_next_line(const int fd, char **line)
+int             get_next_line(const int fd, char **line)
 {
 	static char	*stack[FD_MAX];
-	char		buffer[BUFF_SIZE + 1];
-	int			amount;
+	char        buffer[BUFF_SIZE + 1];
+	int         amount;
 
 	if (fd < 0 || read(fd, buffer, 0) || !line || fd > FD_MAX)
 		return (-1);
@@ -73,7 +73,7 @@ int				get_next_line(const int fd, char **line)
 		amount = read(fd, buffer, BUFF_SIZE);
 		buffer[amount] = '\0';
 		if (!amount)
-			break ;
+            break ;
 		generate_new_string(&(stack[fd]), buffer);
 	}
 	return (generate_new_line(&(stack[fd]), line));
